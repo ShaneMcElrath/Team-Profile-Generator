2cb98512c5a3ab7b287c43272cc200f7216e0eae
Shane McElrath <shane@rangermac.com>
2022-03-04 16:33:32 -0500

    Grabbed generate-site.js for module 9 class work. Got rid of unused code inside of index.js. Create Employee.test.js to test Employee.js. Renamed old employee.js file to employee-old.js. Created new Employee.js file that holds a new employee class. Renamed Engineer.js, Intern.js, and Manager.js to Engineer-old.js, Intern-old.js, and Manager-old.js.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,20 +1,18 @@
 const Employee = require('../lib/Employee');
 
+test('creates an employee object', () => {
+  const employee = new Employee('Robby', 1, 'robby@gmail.com');
 
-test('prompts user with quetions', done => {
-    const employee = new Employee();
-
-    try {
-      let hello = employee.promtQuestions();      
-      console.log(hello);  
-      done();   
-    }
-    catch (err) {    
-     console.log(err);
-     done(error);
-    }    
-  
-
-
-
+  expect(employee.name).toBe('Robby');
+  expect(employee.id).toBe(1);
+  expect(employee.email).toBe('robby@gmail.com');
 });
+
+test('gets employee name, id, email, and role using get functions', () => {
+  const employee = new Employee('Robby', 1, 'robby@gmail.com');
+
+  expect(employee.getName()).toBe('Robby');
+  expect(employee.getId()).toBe(1);
+  expect(employee.getEmail()).toBe('robby@gmail.com');
+  expect(employee.getRole()).toBe('Employee');
+})
