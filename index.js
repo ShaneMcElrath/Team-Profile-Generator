0bfc255a33ef41d9055520cfc64772092fbea848
Shane McElrath <shane@rangermac.com>
2022-03-05 18:07:07 -0500

    Created manager prompt function for prompting all manager questions.

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const {writeFile, copyFile} = require('./utils/generate-site');
 const promptQuetions = require('./utils/prompt-questions');
 
+// Prompts user with questions about the manager then returns whether or
+// not the user wants to add another employee.
+let addNew = promptQuetions.managerPrompt();
 
-
-
-
+while(addNew) {
+  break;
+}
 
   
--- utils/prompt-questions.js
+++ utils/prompt-questions.js
@@ -2,14 +2,18 @@ const inquirer = require('inquirer');
 const questions = require('./questions');
 
 const promptQuestions = {
-  managerPrompt() {
+  async managerPrompt() {
     // Changes base Questions to have the word manager in them.
     questions.updateQuestions('Manager');
 
-    // Promps user with base questions.
-    let managerInfo = inquirer.prompt(questions.baseQuestions);
+    // Prompts user with base questions.
+    let baseManagerInfo = await inquirer.prompt(questions.baseQuestions);
 
+    // Prompts user with manager specific questions.
+    let specificManagerInfo = await inquirer.prompt(questions.managerQuestion);
 
+    // Asks user if they want to add another employee and return there anwser. 
+    return await inquirer.prompt(questions.addEmployeeQuestion);
   }
 }
 
